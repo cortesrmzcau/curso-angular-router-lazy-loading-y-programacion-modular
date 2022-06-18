0823c27e3eecca1b60773d9f661684551dc94792
cortesrmzcau <cesar.a.cortes.94@gmail.com>
2022-06-17 19:14:29 -0500

    3 Paginación de categorías

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/models/product.model';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-category',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  categoryId: string | null = null;
+  products: Product[] = [];
+  limit = 10;
+  offset = 0;
+
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductsService
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.categoryId = params.get('id');
+      if (this.categoryId) {
+        this.productsService.getByCategory(this.categoryId, this.limit, this.offset)
+          .subscribe(data => {
+            this.products = data;
+          })
+      }
+    })
   }
 
 }
