5a58a1a175d4f817886df0e781b3046dde957606
cortesrmzcau <cesar.a.cortes.94@gmail.com>
2022-06-19 21:48:01 -0500

    13 AllModules y CustomStrategy

--- src/app/website/website-routing.module.ts
+++ src/app/website/website-routing.module.ts
@@ -3,13 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LayoutComponent } from './components/layout/layout.component';
 import { HomeComponent } from './pages/home/home.component';
-import { CategoryComponent } from './pages/category/category.component';
 import { MyCartComponent } from './pages/my-cart/my-cart.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { RecoveryComponent } from './pages/recovery/recovery.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { CustomPreloadService } from '../services/custom-preload.service';
 
 const routes: Routes = [
   {
@@ -32,7 +32,10 @@ const routes: Routes = [
       },
       {
         path: 'category',
-        loadChildren: () => import('./pages/category/category.module').then(m => m.CategoryModule)
+        loadChildren: () => import('./pages/category/category.module').then(m => m.CategoryModule),
+        data: {
+          preload: true
+        }
       },
       {
         path: 'product/:id',
