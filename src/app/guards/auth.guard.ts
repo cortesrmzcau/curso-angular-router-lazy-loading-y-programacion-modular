49b6d0e7de7a4c02c861d53a44d702159c1e8f4b
cortesrmzcau <cesar.a.cortes.94@gmail.com>
2022-06-21 13:57:12 -0500

    17 Estado del login

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
 
 import { TokenService } from '../services/token.service';
 
@@ -12,6 +14,7 @@ export class AuthGuard implements CanActivate {
 
   constructor(
     private tokenService: TokenService,
+    private authService: AuthService,
     private router: Router
   ) {
 
@@ -20,12 +23,22 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    const token = this.tokenService.getToken();
-    if (!token) {
-      this.router.navigate(['/home']);
-      return false;
-    }
-    return true;
+    // const token = this.tokenService.getToken();
+    // if (!token) {
+    //   this.router.navigate(['/home']);
+    //   return false;
+    // }
+    // return true;
+    return this.authService.user$
+      .pipe(
+        map(user => {
+          if (!user) {
+            this.router.navigate(['/home']);
+            return false;
+          }
+          return true;
+        })
+      )
   }
 
 }
