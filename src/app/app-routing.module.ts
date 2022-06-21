71999e6f837fbb969aaed6030753dd72e4eeb809
cortesrmzcau <cesar.a.cortes.94@gmail.com>
2022-06-21 14:14:21 -0500

    18 Guard para Admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { QuicklinkStrategy } from 'ngx-quicklink';
 import { NotFoundComponent } from './cms/not-found/not-found.component';
+import { AdminGuard } from './guards/admin.guard';
 import { CustomPreloadService } from './services/custom-preload.service';
 
 const routes: Routes = [
@@ -14,6 +15,7 @@ const routes: Routes = [
   },
   {
     path: 'cms',
+    canActivate: [AdminGuard],
     loadChildren: () => import('./cms/cms.module').then(m => m.CmsModule)
   },
   {
